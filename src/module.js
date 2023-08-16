27176a6c2bc6ad0f8b9c6071decfc80790c1ea59
GagoEgh <egagik69@gmail.com>
2023-08-16 10:33:53 +0400

    update projeckt

--- src/module.js
+++
@@ -1,7 +0,0 @@
-console.log('module')
-
-async function as(){
-  return await Promise.resolve('as')
-}
-
-as().then((result)=>{console.log('then',result)})
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require("path");
 
 const isProd = process.env.NODE_ENV === "production";
 const isDev = !isProd;
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: "development",
   entry: "./index.js",
   output: {
-    filename: filename('js'),
+    filename: filename("js"),
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
@@ -23,10 +24,10 @@ module.exports = {
       "@core": path.resolve(__dirname, "src/core"),
     },
   },
-  devtool: isDev ? 'source-map' : false,
+  devtool: isDev ? "source-map" : false,
   devServer: {
     port: 3000,
-    hot: isDev
+    hot: isDev,
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -34,8 +35,8 @@ module.exports = {
       template: "index.html",
       minify: {
         removeComments: isProd,
-        collapseWhitespace: isProd
-      }
+        collapseWhitespace: isProd,
+      },
     }),
     new CopyPlugin({
       patterns: [
@@ -46,7 +47,7 @@ module.exports = {
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: filename('css')
+      filename: filename("css"),
     }),
   ],
   module: {
